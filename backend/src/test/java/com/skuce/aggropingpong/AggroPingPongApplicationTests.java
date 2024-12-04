90daa067bbc9356bc61708b98a08a0e04639b8bb
조시훈 <kepler452k@naver.com>
2024-12-04 18:31:31 +0900

    feat: Initial Project BE

---
+++ backend/src/test/java/com/skuce/aggropingpong/AggroPingPongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skuce.aggropingpong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggroPingPongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
