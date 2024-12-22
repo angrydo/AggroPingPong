9762e5a1ecce59ee501610faa386112014466aa2
정영도 <jyd12190@gmail.com>
2024-12-22 17:09:27 +0900

    feat: separation of reading and writing functions

--- backend/src/main/java/com/skuce/aggropingpong/user/service/UserReadService.java
+++ backend/src/main/java/com/skuce/aggropingpong/user/service/UserReadService.java
@@ -1,6 +1,5 @@
 package com.skuce.aggropingpong.user.service;
 
-import com.skuce.aggropingpong.user.dto.UserRequestDto;
 import com.skuce.aggropingpong.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+@Transactional
+public class UserReadService {
     private final UserRepository userRepository;
-
-    @Transactional
-    public void create(UserRequestDto userRequestDto) {
-        //수정필요!!!
-    }
-    @Transactional
-    public void update( userRequestDto RequestDto) {
-
-    }
 }
