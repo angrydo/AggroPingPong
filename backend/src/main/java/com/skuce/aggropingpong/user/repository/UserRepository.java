e8bca4a70951b023a221247bf54b035b0b528977
YoungDo <jyd12190@gmail.com>
2025-01-04 17:13:15 +0900

    refactor: remove useless comments

--- backend/src/main/java/com/skuce/aggropingpong/user/repository/UserRepository.java
+++ backend/src/main/java/com/skuce/aggropingpong/user/repository/UserRepository.java
@@ -10,12 +10,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // ENTITY #설명필요!!!
     Optional<User> findByUsername(String username);
     List<User> findAllByName(String name, Sort sort);
     Page<User> findAllByName(String name, Pageable pageable);
 
-    // BOOLEAN #설명필요!!!
     Boolean existsByName(String name);
     Boolean existsByUsername(String username);
 }
