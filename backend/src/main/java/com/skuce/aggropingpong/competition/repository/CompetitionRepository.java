86bd6373031bd40c3f2ce9192cc2858a1e3d3b94
YoungDo <jyd12190@gmail.com>
2025-01-06 17:42:59 +0900

    refactor: Deleted useless imported class file

--- backend/src/main/java/com/skuce/aggropingpong/competition/repository/CompetitionRepository.java
+++ backend/src/main/java/com/skuce/aggropingpong/competition/repository/CompetitionRepository.java
@@ -2,7 +2,6 @@ package com.skuce.aggropingpong.competition.repository;
 
 import com.skuce.aggropingpong.competition.domain.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
