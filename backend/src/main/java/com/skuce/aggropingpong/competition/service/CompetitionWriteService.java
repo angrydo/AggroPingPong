94f68d4647e33f393fec539c0cedb43d32581268
YoungDo <jyd12190@gmail.com>
2025-01-07 14:57:38 +0900

    feat: Add function update, delete

--- backend/src/main/java/com/skuce/aggropingpong/competition/service/CompetitionWriteService.java
+++ backend/src/main/java/com/skuce/aggropingpong/competition/service/CompetitionWriteService.java
@@ -1,8 +1,11 @@
 package com.skuce.aggropingpong.competition.service;
 
+import com.skuce.aggropingpong.common.exception.CustomNotFoundException;
 import com.skuce.aggropingpong.common.exception.CustomValidateException;
+import com.skuce.aggropingpong.competition.domain.Competition;
 import com.skuce.aggropingpong.competition.dto.CompetitionCreateRequestDto;
 import com.skuce.aggropingpong.competition.dto.CompetitionDetailResponseDto;
+import com.skuce.aggropingpong.competition.dto.CompetitionUpdateRequestDto;
 import com.skuce.aggropingpong.competition.repository.CompetitionRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +22,21 @@ public class CompetitionWriteService {
         if (requestDto.getName() == null || requestDto.getName().isEmpty())
             throw new CustomValidateException("The competition name must not be empty");
         return new CompetitionDetailResponseDto(competitionRepository.save(requestDto.toEntity()));
+    }
+
+    public CompetitionDetailResponseDto update(CompetitionUpdateRequestDto requestDto) {
+        if (requestDto.getName() == null || requestDto.getName().isEmpty())
+            throw new CustomValidateException("The competition name must not be empty.");
+
+        Competition entity = competitionRepository.findById(requestDto.getId()).orElseThrow(
+                () -> new CustomNotFoundException("competition not found with id: " + requestDto.getId()));
+        return new CompetitionDetailResponseDto(competitionRepository.save(entity.update(requestDto)));
+    }
+
+    public CompetitionDetailResponseDto delete(Long id) {
+        Competition entity = competitionRepository.findById(id).orElseThrow(
+                () -> new CustomNotFoundException("competition not found with id: " + id));
+        competitionRepository.deleteById(id);
+        return new CompetitionDetailResponseDto(entity);
     }
 }
